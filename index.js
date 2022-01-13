590b4a9f3614e4f63952ff1ac9dd5b7714d785eb
BuendiaBhelle <BuendiaBhelle@gmail.com>
2022-01-13 08:47:33 -0700

    UPDATES: shortens code

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ async function writeToGoogleSheets() {
     const googleSheets = google.sheets({ version: "v4", auth: client })
     const spreadsheetId = process.env.SPREADSHEETID;
 
+    const driver = await new Builder().forBrowser("chrome").build();
+    const pagespeed_url = "https://developers.google.com/speed/pagespeed/insights/";
+
     const monthNames = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12];
     const dateObj = new Date();
     const month = monthNames[dateObj.getMonth()];
@@ -39,7 +42,7 @@ async function writeToGoogleSheets() {
         "https://www.everythingjustrocks.com/",
     ]
     
-    for (let j = 0; j < urls.length; j++) {
+    for (let index = 0; index < urls.length; index++) {
         let ranges = [
             "ACC!A4:C4",
             "SJ!A4:C4",
@@ -54,7 +57,7 @@ async function writeToGoogleSheets() {
 
         let values = [
             [
-                urls[j],
+                urls[index],
                 output,
                 strTime
             ],
@@ -64,7 +67,7 @@ async function writeToGoogleSheets() {
             values,
         };
 
-        const range = ranges[j];
+        const range = ranges[index];
 
         await googleSheets.spreadsheets.values.append({
             auth,
@@ -74,140 +77,10 @@ async function writeToGoogleSheets() {
             resource: resource
         });
         console.log(range);
-    }
-}
-writeToGoogleSheets();
 
-
-async function monitoringNitropack() {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
-    });
-
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({ version: "v4", auth: client })
-    const spreadsheetId = process.env.SPREADSHEETID;
-
-    let driver = await new Builder().forBrowser("chrome").build();
-    
-    const urls = [
-        "https://www.collisioncenternorthscottsdale.com/",
-        "https://www.sunrisejewelryusa.com/",
-        "https://www.americanleatherusa.com/",
-        "https://www.primeview.com/",
-        "https://www.biltmoreloanandjewelry.com/",
-        "https://www.lignans.net/",
-        "https://www.newhopemedicalcenter.com/",
-        "https://www.freddabranyon.com/",
-        "https://www.everythingjustrocks.com/",
-    ]
-
-    for (let index = 0; index < urls.length; index++) {
-        await driver.get("https://developers.google.com/speed/pagespeed/insights/");
+        await driver.get(pagespeed_url);
         await driver.findElement(By.name("url")).sendKeys(urls[index], Key.RETURN);
-        
-        await driver.sleep(40000);
-
-        await driver.executeScript("return document.getElementsByClassName('VfPpkd-YVzG2b')[0].click()");
-
-        const score_length = await driver.executeScript("return document.getElementsByClassName('lh-gauge__percentage').length");
-
-        console.log("IN DESKTOP.");
-
-        console.log(urls[index]);
-
-        for (let i = 0; i < score_length; i++) {
-            const score_desktop = await driver.executeScript("return document.getElementsByClassName('lh-gauge__percentage')[" + i + "].innerText");
-            
-            if (score_desktop != "") {
-                console.log("Desktop: " + score_desktop);
-
-                let ranges_desktop = [
-                    "ACC!D4",
-                    "SJ!D4",
-                    "AL!D4",
-                    "PV!D4",
-                    "BLJ!D4",
-                    "Lignans!D4",
-                    "NHU!D4",
-                    "Fredda Branyon!D4",
-                    "EJR!D4",
-                ]
-
-                let values = [
-                    [
-                        score_desktop
-                    ],
-                ];
-            
-                const resource_desktop = {
-                    values,
-                };
-
-                const range_desktop = ranges_desktop[index];
-            
-                await googleSheets.spreadsheets.values.append({
-                    auth,
-                    spreadsheetId,
-                    range: range_desktop,
-                    valueInputOption: "USER_ENTERED",
-                    resource: resource_desktop
-                });               
-            }
-        }
-
-        await driver.sleep(10000);
-
-        await driver.executeScript("return document.getElementsByClassName('VfPpkd-YVzG2b')[1].click()");
-
-        console.log("IN MOBILE.");
-
-        console.log(urls[index]);
-
-        for (let j = 0; j < score_length; j++) {
-            const score_mobile = await driver.executeScript("return document.getElementsByClassName('lh-gauge__percentage')[" + j + "].innerText");
-            
-            if (score_mobile != "") {
-                console.log("Mobile: " + score_mobile);
-
-                let ranges_mobile = [
-                    "ACC!E4",
-                    "SJ!E4",
-                    "AL!E4",
-                    "PV!E4",
-                    "BLJ!E4",
-                    "Lignans!E4",
-                    "NHU!E4",
-                    "Fredda Branyon!E4",
-                    "EJR!E4",
-                ]
-
-                let values = [
-                    [
-                        score_mobile
-                    ],
-                ];
-            
-                const resource_mobile = {
-                    values,
-                };
-
-                const range_mobile = ranges_mobile[index];
-            
-                await googleSheets.spreadsheets.values.append({
-                    auth,
-                    spreadsheetId,
-                    range: range_mobile,
-                    valueInputOption: "USER_ENTERED",
-                    resource: resource_mobile
-                });               
-            }
-        }
-
-        await driver.sleep(3000);
-
         await driver.switchTo().newWindow('tab');
     }
 }
-monitoringNitropack();
+writeToGoogleSheets();
